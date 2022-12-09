f9b80da2b235d91ae2858595c4cb66936f8f1272
Ssekyene <robertssekyene05@gmail.com>
2022-12-09 07:31:51 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*puts: Fuction to print a string
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle, followed by a new line.\n");
+
+return (0);
+}
