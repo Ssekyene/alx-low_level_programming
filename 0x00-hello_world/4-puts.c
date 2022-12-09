045b9580c3172ac230d5166549153cac16e430d9
Ssekyene <robertssekyene05@gmail.com>
2022-12-09 08:04:27 -0800

    hey puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-puts("\"Programming is like building a");
-puts(" multilingual puzzle, followed by a new line.\n");
+puts("\"Programming is like building a multilingual puzzle,");
+puts(" followed by a new line.\n");
 return (0);
 }
