3b0dc1d129cc113106b940b81c28b7dfd58b0f87
Ssekyene <robertssekyene05@gmail.com>
2023-12-20 13:31:02 -0800

    added 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prints table of parameter
+ * @n: maximum number for getting multiples
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j, multiple;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				multiple = i * j;
+				if (n == 0)
+					printf("%d\n", multiple);
+				if (j != n)
+				{
+					if (j == 0) /*the multiple will always be 0 when j=0*/
+						printf("%d,", multiple);
+					else
+						printf("%4d,", multiple);
+				}
+				else
+					printf("%4d\n", multiple);
+			}
+		}
+	}
+}
