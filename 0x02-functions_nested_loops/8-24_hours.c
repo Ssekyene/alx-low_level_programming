78f03c3feaebfbe4fbf2cd3a7c4b62b936515e50
Ssekyene <robertssekyene05@gmail.com>
2023-12-20 07:48:56 -0800

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: void (Nothing)
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l, flag;
+
+	flag = 0;
+	for (i = 0; i <= 2 && !flag; i++)
+	{
+		for (j = 0; j <= 9 && !flag; j++)
+		{
+			for (k = 0; k <= 5 && !flag; k++)
+			{
+				for (l = 0; l <= 9 && !flag; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+					if (i == 2 && j == 3 && k == 5 && l == 9)
+						flag = 1;
+				}
+			}
+		}
+	}
+}
