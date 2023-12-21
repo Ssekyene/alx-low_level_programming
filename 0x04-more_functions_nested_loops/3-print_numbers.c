8dbf0349da01105e220f21f4df0c431113e815bc
Ssekyene <robertssekyene05@gmail.com>
2023-12-21 13:32:09 -0800

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,5 @@
 1. Checks for a digit (0 through 9).
 
 2. Multiplies two integers.
+
+3. Prints the numbers, from 0 to 9, followed by a new line.
