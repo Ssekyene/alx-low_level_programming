f61647bc03e44b1630e5d203dc9c145ed354592f
Ssekyene <robertssekyene05@gmail.com>
2024-01-04 15:38:07 -0800

    changed 32 to ' '

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32; /*lowercase - 32 = uppercase*/
+			str[i] -= ' '; /*lowercase - 32 = uppercase*/
 	}
 	return (str);
 }
