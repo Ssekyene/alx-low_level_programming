edc78318be19ae3f7ca68891ce7c57fa8259aaec
Ssekyene <robertssekyene05@gmail.com>
2024-01-10 15:06:00 -0800

    added 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - finds length of a string
+ * @s: string parameter
+ * Return: length of parameter
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -6,3 +6,5 @@
 0. Priints a string, followed by a new line.
 
 1. Prints a string in reverse.
+
+2. Returns the length of a string.
