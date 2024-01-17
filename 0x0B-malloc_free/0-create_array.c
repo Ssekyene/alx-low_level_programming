4d7abe3d29329afffb5f1fb3a0f8b40b1d4a9120
Ssekyene <robertssekyene05@gmail.com>
2024-01-17 12:05:23 -0800

    added the initial files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - create an array using malloc()
+ * @size: number of elements in array
+ * @c: char to initialize memory with
+ * Return: initialized array of size size
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr_array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr_array = malloc(size * sizeof(char));
+	if (ptr_array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size - 1; i++)
+		*(ptr_array + i) = c;
+	*(ptr_array + i) = '\0';
+	return (ptr_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+# C - malloc, free
+This directory includes programs with functions that implement malloc and free which are used in dynamic allocation and deallocation of memory.
+
+1. Creates an array of chars, and initializes it with a specific char.
