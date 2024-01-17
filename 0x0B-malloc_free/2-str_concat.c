09e42d86db0314e79bc696510e7115c31d4ae106
Ssekyene <robertssekyene05@gmail.com>
2024-01-17 12:34:28 -0800

    added 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string parameter
+ * @s2: second string parameter
+ * Return: - pointer to concatenated string
+ *	     - NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, size_s1, length = 0;
+	char *ptr_str_concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		length++;
+	for (i = 0; *(s2 + i) != '\0'; i++)
+		length++;
+
+	ptr_str_concat = malloc(sizeof(char) * (length + 1));
+
+	if (ptr_str_concat == NULL)
+		return (NULL);
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(ptr_str_concat + i) = *(s1 + i);
+	size_s1 = i;
+	for (i = 0; *(s2 + i) != '\0'; i++)
+		*(ptr_str_concat + size_s1 + i) = *(s2 + i);
+	*(ptr_str_concat + size_s1 + i) = '\0';
+
+	return (ptr_str_concat);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,3 +4,5 @@ This directory includes programs with functions that implement malloc and free w
 0. Creates an array of chars, and initializes it with a specific char.
 
 1. Returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+2. Concatenates two strings.
