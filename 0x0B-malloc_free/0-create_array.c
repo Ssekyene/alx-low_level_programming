7415fd400b85bc50ae2f4ad841a1c9c5917cf31d
Ssekyene <robertssekyene05@gmail.com>
2024-01-17 12:09:51 -0800

    fixed the loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (ptr_array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 		*(ptr_array + i) = c;
 	*(ptr_array + i) = '\0';
 	return (ptr_array);
