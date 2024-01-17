8c7f77b07d7ef2e37bcb3153308360cdcbfa3793
Ssekyene <robertssekyene05@gmail.com>
2024-01-17 12:58:27 -0800

    added 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - make a 2D array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return:  a pointer to a 2 dimensional array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr_grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr_grid = (int **) malloc(sizeof(int *) * height);
+
+	if (ptr_grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		*(ptr_grid + i) = (int *) malloc(sizeof(int) * width);
+		if (*(ptr_grid + i) == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(*(ptr_grid + j));
+			free(ptr_grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			*(*(ptr_grid + i) + j) = 0;
+	}
+
+	return (ptr_grid);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -6,3 +6,5 @@ This directory includes programs with functions that implement malloc and free w
 1. Returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 
 2. Concatenates two strings.
+
+3. Returns a pointer to a 2 dimensional array of integers.
