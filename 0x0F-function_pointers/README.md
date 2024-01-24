dcd35c2beab862078a2e6a207747357f3ee4dfe9
Ssekyene <robertssekyene05@gmail.com>
2024-01-24 11:26:23 -0800

    first push

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+# C - Function pointers
+**This directory includes functions that implement pointers to functions:**
+
+0. Prints a name.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_PTRS_H
+#define FUNC_PTRS_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
+
