f216045888e71a4b5f084ec47a1162657ae4c9d7
Ssekyene <robertssekyene05@gmail.com>
2024-01-25 03:46:39 -0800

    added a simple calculator program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point for the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int result, num1, num2;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op_func = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
