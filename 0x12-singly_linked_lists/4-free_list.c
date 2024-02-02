b285f50ad0e5832c20b83fd263e9495835e1c6d3
Ssekyene <robertssekyene05@gmail.com>
2024-02-02 14:01:19 -0800

    added 4-free_lists.h

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - frees memory allocated to singly linked list
+ * @head: pointer to first node
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -10,3 +10,5 @@
 2. Adds a new node at the beginning of a `list_t` list.
 
 3. Adds a new node at the end of a `list_t` list.
+
+4. Frees a `list_t` list.
