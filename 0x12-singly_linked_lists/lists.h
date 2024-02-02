67eff6ba92434199453535227176c6f86f0da700
Ssekyene <robertssekyene05@gmail.com>
2024-02-02 10:34:56 -0800

    included standard files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+/***STANDARD HEADER FILES ***/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
